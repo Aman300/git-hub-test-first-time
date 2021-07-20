647eba8d1b9a83d4c3a06d0e553146228ba75d93
Aman300 <87026675+Aman300@users.noreply.github.com>
2021-07-20 12:07:37 +0530

    Update 3 number 1 number big 2.cpp

--- 3 number 1 number big 2.cpp
+++ 3 number 1 number big 2.cpp
@@ -1,22 +1,16 @@
 #include<iostream>
 using namespace std;
-int big(int number1,int number2,int number3)
-{
-	int n1,n2,n3;
-		cout << "Enter the three number:";
-		cin >> n1 >> n2 >> n3 ;
-		if(n1>n2&&n1>n3)
-			cout << n1 <<" is big";
-		else if(n2>n1&&n2>n3)
-			cout << n2 << " is big";
-		else
-			cout << n3 << " is big";
-	return 0;	
-}
 int main()
 {
-	int num1,num2,num3;
-	big(num1,num2,num3);
+	int n1,n2,n3;
+	cout<<"Enter the three number:";
+	cin>>n1>>n2>>n3;
+	int res;
+	if(n1>n2&&n1>n3)
+		cout<<n1<<" is big:";
+	else if(n2>n3&&n2>n1)
+		cout<<n2<<" is big:";
+	else
+		cout<<n3<<" is big:";
 	return 0;
 }
-
